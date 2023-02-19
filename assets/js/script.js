821bcc5976c492e86562922bbc033665793181be
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-02-19 15:44:31 -0500

    questions and answers now showing up on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 var question = document.querySelector("#question");
-var choices = document.querySelectorAll(".answer-text");
+var choices = Array.from(document.querySelectorAll(".answer-text"));
 var timer = document.querySelector("#timer-value");
 var btn = document.querySelectorAll(".btn");
+var scoreText = document.querySelector("#score");
 
-var currentQuestion= "";
+var currentQuestion= {};
 var questionCounter = 0;
 var availableQuestions = [];
 
@@ -26,91 +27,104 @@ var countDown = setInterval(function() {
     }
 }, 1000);
 
-// var questions = [
-//     {
-//         question: "Commonly used data types DO NOT include ___.",
-//         firstChoice: "strings",
-//         secondChoice: "booleans",
-//         thirdChoice: "alerts",
-//         fourthChoice: "numbers",
-//         answer: 3,
-//     },
-//     {
-//         question: "The condition in an if/else statement is enclosed with _____.",
-//         firstChoice: "quotes",
-//         secondChoice: "curly brackets",
-//         thirdChoice: "parentheses",
-//         fourthChoice: "square brackets",
-//         answer: 2,
-//     },
-//     {
-//         question: "Arrays in JavaScript can be used to store ____.",
-//         firstChoice: "numbers and strings",
-//         secondChoice: "other arrays",
-//         thirdChoice: "booleans",
-//         fourthChoice: "all of the above",
-//         answer: 4,
-//     },
-//     {
-//         question: "String values must be enclosed within ____ when being assigned to variables.",
-//         firstChoice: "commas",
-//         secondChoice: "curly brackets",
-//         thirdChoice: "quotes",
-//         fourthChoice: "parentheses",
-//         answer: 3,
-//     },
-//     {
-//         question: "A very useful tool using during development and debugging for printing content to the debugger is ____.",
-//         firstChoice: "JavaScript",
-//         secondChoice: "terminal / bash",
-//         thirdChoice: "for loops",
-//         fourthChoice: "console.log()",
-//         answer: 4,
-//     }
-// ]
+ var questions = [
+     {
+         question: "Commonly used data types DO NOT include ___.",
+         choice1: "strings",
+         choice2: "booleans",
+         choice3: "alerts",
+         choice4: "numbers",
+         answer: 3,
+     },
+     {
+        question: "The condition in an if/else statement is enclosed with _____.",
+         choice1: "quotes",
+         choice2: "curly brackets",
+         choice3: "parentheses",
+         choice4: "square brackets",
+         answer: 2,
+     },
+     {
+         question: "Arrays in JavaScript can be used to store ____.",
+         choice1: "numbers and strings",
+         choice2: "other arrays",
+         choice3: "booleans",
+         choice4: "all of the above",
+         answer: 4,
+     },
+     {
+         question: "String values must be enclosed within ____ when being assigned to variables.",
+         firstChoice: "commas",
+         secondChoice: "curly brackets",
+         thirdChoice: "quotes",
+         fourthChoice: "parentheses",
+         answer: 3,
+     },
+     {
+         question: "A very useful tool using during development and debugging for printing content to the debugger is ____.",
+         choice1: "JavaScript",
+         choice2: "terminal / bash",
+         choice3: "for loops",
+         choice4: "console.log()",
+         answer: 4,
+     }
+ ]
 
-// var score = 20;
-// var maxQuestions = 5;
+ var score = 20;
+ var maxQuestions = 5;
 
-// var startGame = function() {
-// questionCounter = 0;
-// score = 0;
-// availableQuestions = [...questions];
-// getNewQuestion()
-// }
+ var startGame = function() {
+ questionCounter = 0;
+ score = 0;
+ availableQuestions = [...questions];
+ getNewQuestion();
+ };
 
-// getNewQuestion = function() {
-//     if(availableQuestions.length === 0 || questionCounter > maxQuestions) {
-//         localStorage.setItem('mostRecentScore', score)
+var getNewQuestion = function() {
+     if(availableQuestions.length === 0 || questionCounter > maxQuestions) {
+         localStorage.setItem('mostRecentScore', score);
 
-//         //this might have to be different
-//         return window.location.assign('/end.html')
-//     }
+         return window.location.assign('/end.html');
+     };
 
-//     questionCounter++;
+     questionCounter++;
 
-//     var questionIndex = Math.floor(Math.random() * availableQuestions.length);
-//     currentQuestion = availableQuestions[questionIndex];
-//     question.innerText = currentQuestion.question;
+     var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+     currentQuestion = availableQuestions[questionIndex];
+     question.innerText = currentQuestion.question;
 
-//     choices.forEach(function(choice) {
-//         var number = choice.dataset('number');
-//         choice.innerText = currentQuestion['choice' + number];
-//     });
+     choices.forEach(function(choice) {
+         var number = choice.dataset['number'];
+         choice.innerText = currentQuestion['choice' + number];
+     });
 
-//     availableQuestions.splice(questionIndex, 1);
-// }
+    availableQuestions.splice(questionIndex, 1);
+ };
 
-// choices.forEach(function(choice) {
-//     choice.addEventListener('click', function(event) { 
-//         var selectedChoice = event.target;
-//         var selectedAnswer = selectedChoice.dataset['number'];
 
-//         if (selectedAnswer == currentQuestion.answer) {
-//             var message = document.querySelector(".hidden");
-//             message.textContent = "Correct!";
-//         } else {
-//             message.textContent = "Wrong!";
-//         }
-//     });
-// })
+ choices.forEach(function(choice) {
+     choice.addEventListener('click', function(event) { 
+         var selectedChoice = event.target;
+        var selectedAnswer = selectedChoice.dataset['number'];
+
+         if (selectedAnswer == currentQuestion.answer) {
+             var message = document.querySelector(".hidden");
+             message.textContent = "Correct!";
+             
+             incrementScore(score);
+         } else {
+             message.textContent = "Wrong!";
+
+             //include detract time
+         }
+
+         getNewQuestion()
+    });
+ });
+
+ incrementScore = function(num) {
+    score +=num;
+    scoreText.innerText = score;
+ }
+
+ startGame();
