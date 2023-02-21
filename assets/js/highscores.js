52dcdeb2d89fc49e0164594a6a975706b8495b59
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-02-20 21:22:17 -0500

    Initials and score now showing up as regular text. However, it only holds one score.

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -5,10 +5,14 @@ var highScores = JSON.parse(localStorage.getItem("highScores"));
 
 var splitHighScores = Object.values(highScores);
 
+var stringSplitHighScores = splitHighScores.toString();
+
+var values = stringSplitHighScores.split(",", 2);
+
 function renderHighScoresList() {
     var listEl = document.createElement("li");
     highScoresList.appendChild(listEl);
-    listEl.innerText = splitHighScores;
+    listEl.innerText = values[0] + " - " + values[1];
 }
 
 renderHighScoresList();
