3b8370d851875ff5bcd14ca6d5e89dec7ff06aa6
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-02-20 20:49:57 -0500

    Stored initials and score now displaying, but not stylized

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,8 +1,14 @@
 var highScoresList = document.querySelector("#high-scores-list");
 var highScores = JSON.parse(localStorage.getItem("highScores"));
 
-console.log(highScores);
-
 //create <li> elements with the saved initials/name and score
-// var highScoreListElement = highScoresList.createElement("li");
-// highScoreListElement.textContent = highScores;
+
+var splitHighScores = Object.values(highScores);
+
+function renderHighScoresList() {
+    var listEl = document.createElement("li");
+    highScoresList.appendChild(listEl);
+    listEl.innerText = splitHighScores;
+}
+
+renderHighScoresList();
